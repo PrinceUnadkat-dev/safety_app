7e3dd6c484e83a24da93c19094b7ccba52d8f25d
PrinceUnadkat <forproject199@gmail.com>
2025-09-05 13:56:21 +0530

    update

--- app/src/main/java/com/example/safety_app/MainActivity.kt
+++ app/src/main/java/com/example/safety_app/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.safety_app.databinding.ActivityMainBinding
-//this
+//another
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
